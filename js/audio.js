68fbf1280f67b93da7be46e410fc559f915884dd
John Varouhakis <johnvarouhakis@gmail.com>
2014-07-23 20:13:55 +0300

    phonegap audio

--- js/audio.js
+++ js/audio.js
@@ -1,4 +1,4 @@
-var audio = {
+var _audioTag = {
   markers: [
     [0,0.3], //roll
     [2,2.3], //move
@@ -55,3 +55,63 @@ var audio = {
   }
 
 };
+
+var _audioCordova = {
+  markers: [
+    [0,300], //roll
+    [2000,2300], //move
+    [4000,4300], //lose life
+    [6000,7600], //game over
+    [9000,12600] //game win
+  ],
+  ready: false,
+  buffer: null,
+  nextMarker: 0,
+  timer: -1,
+  load: function() {
+    if(audio.buffer === null) {
+      audio.buffer = new Media("file:///android_asset/www/audio/sounds.mp3");
+      audio.timer = setInterval(function() {
+        audio.buffer.getCurrentPosition( function(position) {
+          if(position*1000 > audio.nextMarker) {
+            audio.buffer.pause();
+          }
+        });
+      },100);
+      audio.buffer.play();
+    }
+  },
+  play: function(sound) {
+    var limits = null;
+    switch(sound) {
+      case 'roll':
+        limits = audio.markers[0];
+        break;
+      case 'move':
+        limits = audio.markers[1];
+        break;
+      case 'crash':
+        limits = audio.markers[2];
+        break;
+      case 'die':
+        limits = audio.markers[3];
+        break;
+      case 'win':
+        limits = audio.markers[4];
+        break;
+      default:
+        limits = [0,0];
+    }
+    audio.buffer.seekTo(limits[0]);
+    audio.nextMarker = limits[1];
+    audio.buffer.play();
+  }
+};
+
+if(window.cordova) {
+  window.audio = _audioCordova;
+}
+else {
+  window.audio = _audioTag;
+}
+
